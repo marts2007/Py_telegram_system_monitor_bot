3b9a2bf7ba84a5c2d2b4c659807849c1ccf006f7
Dmitry <admin.marts@gmail.com>
2020-05-29 13:06:23 +0300

    fix

--- telegram_bot.py
+++ telegram_bot.py
@@ -70,9 +70,9 @@ class Telegram:
             message = ''
             for drive in config.drives:
                 drv = psutil.disk_usage(drive['path'])
-                message = "Filesystem {} \r\nTotal: {} GiB\r\n".format(drive['path'], drv.total / (2**30))
-                message += "\r\nUsed: {} GiB\r\n".format(drv.used / (2**30))
-                message += "Free: {} GiB\r\n\r\n".format(drv.free / (2**30))
+                message += "Filesystem {} \r\nTotal: {} GiB\r\n".format(round(drive['path'], drv.total / (2**30),1))
+                message += "Used: {} GiB\r\n".format(round(drv.used / (2**30),1))
+                message += "Free: {} GiB\r\n\r\n".format(round(drv.free / (2**30),1))
                 if os.path.isfile(drive['report_file']):
                     message += open(drive['report_file'], 'r').read()
                     message += "\r\n\r\n"
